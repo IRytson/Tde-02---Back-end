f9fff8073b37ff859c893eaaa77aff3a19fdf576
Lázaro Rytson <83834578+IRytson@users.noreply.github.com>
2022-04-27 14:59:50 +0000

    Commit 2.0

---
+++ Twofer.rb
@@ -0,0 +1,5 @@
+class Twofer
+  def self.twofer(name="Você")
+    puts "One for #{name}, one for me."
+  end
+end
---
+++ palavrao.rb
@@ -0,0 +1,9 @@
+class palavrao
+  def palavrinha (palavra)
+    if (palavra_tamanho >= 10)
+      return "Palavrão"
+    elsif 
+      return "palavrinha"
+    end
+  end
+end
